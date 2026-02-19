7659588a8063b4843fd56d24f506ad28eb6d34fa
Shipan Ahmed <shipan.cse.brur@gmail.com>
2026-02-19 17:46:19 +0600

    completed login page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 console.log("Hello, World!");
+
+document.getElementById("login-btn").addEventListener("click", function () {
+    // 1. get the mobile number
+    const mobileNumber = document.getElementById("login-nbr");
+    const mobileNumberValue = mobileNumber.value;
+    console.log(mobileNumberValue);
+
+    // 2. get the password
+    const password = document.getElementById("login-pin");
+    const passwordValue = password.value;
+    console.log(passwordValue);
+
+    // 3. check the mobile number and password is valid or not
+    if (mobileNumberValue === "01776256168" && passwordValue === "1234") {
+        alert("Login successful!");
+        window.location.assign("./home.html");
+    } else {
+        alert("Invalid mobile number or password. Please try again.");
+        return;
+    }
+})
